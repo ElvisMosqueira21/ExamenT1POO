895cca0bee45df4b28a662677775bdfc64a2f8c0
Elvis_M <maynorelvisjcm@gmail.com>
2026-09-21 19:17:53 -0500

    Primer commit: Examen t1 tecnicas poo

---
+++ src/main/java/com/mycompany/exament1tecnicas/ExamenT1Tecnicas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exament1tecnicas;
+
+/**
+ *
+ * @author quant
+ */
+public class ExamenT1Tecnicas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
